4cb9bc1af4856ec00fdcef3c9c1d55950f9fddf8
sasakiy84 <67269692+sasakiy84@users.noreply.github.com>
2023-10-14 00:17:30 +0900

    Update setup_shell.sh

--- setup_shell.sh
+++ setup_shell.sh
@@ -26,4 +26,7 @@ curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.1.1/a
 echo "c2af02bdd15da6794f9c98db40332c804224930212f553a805425441f8331665  aqua-installer" | sha256sum -c
 chmod +x aqua-installer
 ./aqua-installer
+
 export PATH="${AQUA_ROOT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/aquaproj-aqua}/bin:$PATH"
+export AQUA_GLOBAL_CONFIG=${AQUA_GLOBAL_CONFIG:-}:${XDG_CONFIG_HOME:-$HOME/.config}/aquaproj-aqua/aqua.yaml
+aqua i --all --only-link
