bc682e1fa07b2777f58120fdfa134c9dc64fd2ca
sasakiy84 <67269692+sasakiy84@users.noreply.github.com>
2023-10-13 23:46:27 +0900

    Update setup_shell.sh

--- setup_shell.sh
+++ setup_shell.sh
@@ -5,16 +5,16 @@
 # ----------
 GIT_CONFIG_LOCAL=~/.gitconfig.local
 if [ ! -e $GIT_CONFIG_LOCAL ]; then
-	echo -n "git config user.email?> "
-	read GIT_AUTHOR_EMAIL
+        echo -n "git config user.email?> "
+        read GIT_AUTHOR_EMAIL
 
-	echo -n "git config user.name?> "
-	read GIT_AUTHOR_NAME
+        echo -n "git config user.name?> "
+        read GIT_AUTHOR_NAME
 
-	cat < $GIT_CONFIG_LOCAL
+        cat << EOF > $GIT_CONFIG_LOCAL
 [user]
-	name = $GIT_AUTHOR_NAME
- 	email = $GIT_AUTHOR_EMAIL
+        name = $GIT_AUTHOR_NAME
+        email = $GIT_AUTHOR_EMAIL
 EOF
 fi
 
