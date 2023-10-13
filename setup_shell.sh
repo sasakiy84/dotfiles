392a7b7bd074e5042048237d7f8577beecf47cde
sasakiy84 <sincerely.sasakiy84@gmail.com>
2023-10-13 22:58:08 +0900

    setup git config

--- setup_shell.sh
+++ setup_shell.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+# ----------
+# setup git username and email
+# ----------
+GIT_CONFIG_LOCAL=~/.gitconfig.local
+if [ ! -e $GIT_CONFIG_LOCAL ]; then
+	echo -n "git config user.email?> "
+	read GIT_AUTHOR_EMAIL
+
+	echo -n "git config user.name?> "
+	read GIT_AUTHOR_NAME
+
+	cat < $GIT_CONFIG_LOCAL
+[user]
+	name = $GIT_AUTHOR_NAME
+ 	email = $GIT_AUTHOR_EMAIL
+EOF
+fi
 
 # ----------
 # install aqua
